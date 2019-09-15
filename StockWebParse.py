375d07b004cdfafd5b7a0e8d7c930e0de33087aa
Fanzil <fan.hakimov2010@yandex.ru>
2019-09-15 21:41:52 +0300

    fix code style

--- StockWebParse.py
+++ StockWebParse.py
@@ -4,41 +4,50 @@ from datetime import date, datetime
 
 
 def getData(stock, _date):
-    date_string = ''
+    date_string = ""
     try:
-        date_string = '&date=%d' % ((datetime.strptime(_date, '%d%m%Y').date() - date(2018, 8, 3)).total_seconds()
-                                    + 1533254400)
-        _date = '_' + str(_date)
+        date_string = "&date=%d" % (
+            (
+                datetime.strptime(_date, "%d%m%Y").date() - date(2018, 8, 3)
+            ).total_seconds()
+            + 1533254400
+        )
+        _date = "_" + str(_date)
     except ValueError:
-        _date = ''
-    req = get('https://finance.yahoo.com/quote/{0}/options?p={0}{1}'.format(stock, date_string))
+        _date = ""
+    req = get(
+        "https://finance.yahoo.com/quote/{0}/options?p={0}{1}".format(
+            stock, date_string
+        )
+    )
 
-    soup = BeautifulSoup(req.content, 'html5lib')
-    section = soup.find('section', attrs={'class': 'Mt(20px) qsp-2col-options'})
+    soup = BeautifulSoup(req.content, "html5lib")
+    section = soup.find("section", attrs={"class": "Mt(20px) qsp-2col-options"})
 
     try:
-        tbody = section.find('tbody')
-        trows = tbody.find_all('tr')
+        tbody = section.find("tbody")
+        trows = tbody.find_all("tr")
     except AttributeError:
-        print('%s stock is invalid or data for this date have not found' % stock)
+        print("%s stock is invalid or data for this date have not found" % stock)
         return
 
-    file = open('stock_%s%s.csv' % (stock, _date), 'w')
+    file = open("stock_%s%s.csv" % (stock, _date), "w")
 
     for row in trows:
-        cells = row.find_all('td')
+        cells = row.find_all("td")
 
-        res = ','.join([cells[i].text for i in range(11)])
+        res = ",".join([cells[i].text for i in range(11)])
 
-        file.write(res + '\n')
+        file.write(res + "\n")
 
-    print('%s\nDone! Your file is ready. It is called %s\nThe data were taken from link below:\n%s' % ('_' * 62,
-                                                                                                       file.name,
-                                                                                                       req.url))
+    print(
+        "%s\nDone! Your file is ready. It is called %s\nThe data were taken from link below:\n%s"
+        % ("_" * 62, file.name, req.url)
+    )
     file.close()
 
 
-if __name__ == '__main__':
-    stock = input('Enter name of stock...').upper()
+if __name__ == "__main__":
+    stock = input("Enter name of stock...").upper()
     _date = input('Enter date in the format "%dd%mm%YYYY" if necessary or skip...')
     getData(stock, _date)
--- YahooGrab.py
+++ YahooGrab.py
@@ -5,14 +5,25 @@ import re
 
 def parse():
     try:
-        os.mkdir('Yahoo Finance Parsed Data')
+        os.mkdir("Yahoo Finance Parsed Data")
     except FileExistsError:
         pass
-    os.chdir('Yahoo Finance Parsed Data')
-    file = open('stocks_info.csv', 'w')
+    os.chdir("Yahoo Finance Parsed Data")
+    file = open("stocks_info.csv", "w")
 
     # Here is stocks list
-    stocks = ['TSLA', 'DRIP', 'TVIX', 'RUN', 'RIOT', 'KNDI', 'MRO', 'GBTC', 'INGN', 'FAZ']
+    stocks = [
+        "TSLA",
+        "DRIP",
+        "TVIX",
+        "RUN",
+        "RIOT",
+        "KNDI",
+        "MRO",
+        "GBTC",
+        "INGN",
+        "FAZ",
+    ]
     for stock in stocks:
         get_stock_info(stock, file)
         get_options_info(stock)
@@ -27,23 +38,49 @@ def get_stock_info(stock, file):
     :param file: An output filename
     :return:
     """
-    g = Grab(transport='urllib3')
-    g.go('https://finance.yahoo.com/quote/%s' % stock)
+    g = Grab(transport="urllib3")
+    g.go("https://finance.yahoo.com/quote/%s" % stock)
 
-    last_price = g.doc.select('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]').text()
-    change, change_perc = g.doc.select('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[2]').text().split()
-    curr = g.doc.select('//*[@id="quote-header-info"]/div[2]/div[1]/div[2]/span').text()[-3:]
+    last_price = g.doc.select(
+        '//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]'
+    ).text()
+    change, change_perc = (
+        g.doc.select('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[2]')
+        .text()
+        .split()
+    )
+    curr = g.doc.select(
+        '//*[@id="quote-header-info"]/div[2]/div[1]/div[2]/span'
+    ).text()[-3:]
     mark_time = g.doc.select('//*[@id="quote-market-notice"]/span').text()
-    mark_time = re.search(r'\d+:\d+\w\w \w{3}', mark_time, flags=re.ASCII).group(0)
+    mark_time = re.search(r"\d+:\d+\w\w \w{3}", mark_time, flags=re.ASCII).group(0)
     pat = '//*[@id="quote-summary"]/div[1]/table/tbody/tr[%d]/td[2]'
-    vol = g.doc.select(pat % 7).text().replace(',', '.')
-    avg_vol = g.doc.select(pat % 8).text().replace(',', '.')
+    vol = g.doc.select(pat % 7).text().replace(",", ".")
+    avg_vol = g.doc.select(pat % 8).text().replace(",", ".")
     day_r = g.doc.select(pat % 5).text()
     week_r = g.doc.select(pat % 6).text()
-    mark_cap = g.doc.select('//*[@id="quote-summary"]/div[2]/table/tbody/tr[1]/td[2]/span').text()
+    mark_cap = g.doc.select(
+        '//*[@id="quote-summary"]/div[2]/table/tbody/tr[1]/td[2]/span'
+    ).text()
 
-    file.write(stock + ',')
-    file.write(','.join(i for i in [last_price, change, change_perc, curr, mark_time, vol, avg_vol, day_r, week_r, mark_cap, '\n']))
+    file.write(stock + ",")
+    file.write(
+        ",".join(
+            i for i in [
+                last_price,
+                change,
+                change_perc,
+                curr,
+                mark_time,
+                vol,
+                avg_vol,
+                day_r,
+                week_r,
+                mark_cap,
+                "\n",
+            ]
+        )
+    )
 
 
 def get_options_info(stock):
@@ -52,30 +89,35 @@ def get_options_info(stock):
     :param stock: A stock's name
     :return:
     """
-    g = Grab(transport='urllib3')
-    g.go('https://finance.yahoo.com/quote/{0}/options?p={0}'.format(stock))
+    g = Grab(transport="urllib3")
+    g.go("https://finance.yahoo.com/quote/{0}/options?p={0}".format(stock))
 
     try:
-        os.mkdir('Options')
+        os.mkdir("Options")
     except FileExistsError:
         pass
-    file = open('Options/stock_%s.csv' % stock, 'w')
+    file = open("Options/stock_%s.csv" % stock, "w")
 
-    for i, j in zip([1, 2], ['Calls', 'Puts']):
-        file.write('%s\n' % j)
-        rows = g.doc.select('//*[@id="Col1-1-OptionContracts-Proxy"]/section/section[%d]/div[2]/div/table/tbody/tr' % i)
+    for i, j in zip([1, 2], ["Calls", "Puts"]):
+        file.write("%s\n" % j)
+        rows = g.doc.select(
+            '//*[@id="Col1-1-OptionContracts-Proxy"]/section/section[%d]/div[2]/div/table/tbody/tr'
+            % i
+        )
         for row in rows:
-            tds = [td.text() for td in row.select('td')]
-            file.write(','.join(tds) + '\n')
+            tds = [td.text() for td in row.select("td")]
+            file.write(",".join(tds) + "\n")
 
     file.close()
 
 
-if __name__ == '__main__':
-    print('Data is been parsing...Please, wait...')
+if __name__ == "__main__":
+    print("Data is been parsing...Please, wait...")
 
     parse()
 
-    print('%s\nDone! All the output data is in the folder "Yahoo Finance Parsed Data".\nCommon info is in the file '
-          'called stocks_info.csv \nOptions for each stock are in the folder Options.\nIf the options do not exist for '
-          'any stock, then perhaps\n   this info does not exist on the site.' % ('_' * 63))
+    print(
+        '%s\nDone! All the output data is in the folder "Yahoo Finance Parsed Data".\nCommon info is in the file '
+        "called stocks_info.csv \nOptions for each stock are in the folder Options.\nIf the options do not exist for "
+        "any stock, then perhaps\n   this info does not exist on the site." % ("_" * 63)
+    )
